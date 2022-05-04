a4fbc7bd09a57559d3dc617c08d259262b0fef1b
Sergio Gurgel <sergiogurgell@gmail.com>
2022-05-04 11:20:42 -0300

    Página de detalhes de um galpão

---
+++ app/controllers/warehouses_controller.rb
@@ -0,0 +1,7 @@
+class WarehousesController < ApplicationController
+  def show
+    id = params[:id]
+    @warehouse = Warehouse.find(id)
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_02_173928) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_04_133239) do
   create_table "warehouses", force: :cascade do |t|
     t.string "name"
     t.string "code"
@@ -18,6 +18,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_02_173928) do
     t.integer "area"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
+    t.string "postal_code"
+    t.string "description"
   end
 
 end
