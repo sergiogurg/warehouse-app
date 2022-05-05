dd9e2c42d594d9874ade3c87aaf9328f08e76168
Sergio Gurgel <sergiogurgell@gmail.com>
2022-05-05 00:52:18 -0300

    Usuário cadastra um novo galpão com sucesso

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -6,4 +6,17 @@ class WarehousesController < ApplicationController
 
   def new
   end
+
+  def create
+    # Aqui dentro nós vamos:
+    # 1 - Receber os dados enviados no formulário
+    # 2 - Criar um novo galpão no banco de dados
+    warehouse_params = params.require(:warehouse).permit(:name, :description,
+                                                        :code, :address, :city,
+                                                        :state, :postal_code, :area)
+    w = Warehouse.new(warehouse_params)
+    w.save()
+    # 3 - Redirecionar para a tela inicial
+    redirect_to root_path
+  end
 end
--- spec/system/user_register_warehouse_spec.rb
+++ spec/system/user_register_warehouse_spec.rb
@@ -17,6 +17,31 @@ describe 'Usuário cadastra um galpão' do
     expect(page).to have_field('Estado')
     expect(page).to have_field('CEP')
     expect(page).to have_field('Área')
+  end
+
+  it 'com sucesso' do
+    # Arrange
+
+    # Act
+    visit root_path
+    click_on 'Cadastrar Galpão'
+    fill_in 'Nome', with: 'Rio de Janeiro'
+    fill_in 'Descrição', with: 'Galpão da zona portuária do Rio'
+    fill_in 'Código', with: 'RIO'
+    fill_in 'Endereço', with: 'Avenida do Museu do Amanhã, 1000'
+    fill_in 'Cidade', with: 'Rio de Janeiro'
+    fill_in 'CEP', with: '20100-000'
+    fill_in 'Área', with: 32_000
+    click_on 'Enviar'
+
+    # Assert
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content('Rio de Janeiro')
+    expect(page).to have_content('Código')
+    expect(page).to have_content('Cidade')
+    expect(page).to have_content('Estado')
+    expect(page).to have_content('Área')
 
   end
+
 end
