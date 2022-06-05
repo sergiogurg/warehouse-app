59fab5bd3af56456a6329a06a0109264881ebbb8
Sergio Gurgel <sergiogurgell@gmail.com>
2022-06-05 08:47:10 -0300

    Aula 34 - Busca de Pedidos (parte 2)

--- spec/system/orders/user_search_order_spec.rb
+++ spec/system/orders/user_search_order_spec.rb
@@ -48,6 +48,37 @@ describe 'Usuário busca por um pedido' do
     expect(page).to have_content("Código: #{order.code}")
     expect(page).to have_content('Galpão Destino: GRU => Aeroporto SP')
     expect(page).to have_content('Fornecedor: M Dias Branco S.A. Indústria e Comércio de Alimentos')
-    
   end
+
+  it 'e encontra múltiplos pedidos' do
+    # Arrange
+    user = User.create!(name: 'Sergio', email: 'sergio@email.com', password: '12345678')
+    first_warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', state: 'SP', area: 100_000,
+      address: 'Avenida do Aeroporto, 1000', postal_code: '15000-000',
+      description: 'Galpão destinado a cargas internacionais')
+    second_warehouse = Warehouse.create!(name: 'Aeroporto Rio', code: 'SDU', city: 'Rio de Janeiro', state: 'RJ', area: 76_759,
+      address: 'Avenida Tom Jobim, 525', postal_code: '63263-814',
+      description: 'Galpão destinado a cargas internacionais')
+    supplier = Supplier.create!(corporate_name: 'M Dias Branco S.A. Indústria e Comércio de Alimentos', brand_name: 'Fábrica Fortaleza', registration_number: '07206816000115', address: 'BR 116, km 18', city: 'Eusébio', state: 'CE', email: 'sac@mdiasbranco.com.br')
+    allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('GRU12345')
+    first_order = Order.create!(user: user, warehouse: first_warehouse, supplier: supplier, estimated_delivery_date: 1.day.from_now)
+    allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('GRU85195')
+    second_order = Order.create!(user: user, warehouse: first_warehouse, supplier: supplier, estimated_delivery_date: 1.day.from_now)
+    allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('SDU00000')
+    third_order = Order.create!(user: user, warehouse: second_warehouse, supplier: supplier, estimated_delivery_date: 1.day.from_now)
+
+    # Act
+    login_as(user)
+    visit root_path
+    fill_in 'Buscar Pedido', with: 'GRU'
+    click_on 'Buscar'
+
+    # Assert
+    expect(page).to have_content('GRU12345')
+    expect(page).to have_content('GRU85195')
+    expect(page).to have_content('Galpão Destino: GRU => Aeroporto SP')
+    expect(page).not_to have_content('SDU00000')
+    expect(page).not_to have_content('Galpão Destino: SDU => Aeroporto Rio')
+  end
+
 end
