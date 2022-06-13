447a17b39ee908d2d75daff105dfe63945205726
Sergio Gurgel <sergiogurgell@gmail.com>
2022-06-12 22:15:32 -0300

    Aula 45 - APIs: listando todos galpões

--- app/controllers/api/v1/warehouses_controller.rb
+++ app/controllers/api/v1/warehouses_controller.rb
@@ -1,6 +1,16 @@
 class Api::V1::WarehousesController < ActionController::API
   def show
-    warehouse = Warehouse.find(params[:id])
-    render status: 200, json: warehouse
+
+    begin
+      warehouse = Warehouse.find(params[:id])
+      render status: 200, json: warehouse.as_json(except: [:created_at, :updated_at])
+    rescue
+      render status: 404
+    end
+  end
+
+  def index
+    warehouses = Warehouse.all.order(:name)
+    render status: 200, json: warehouses
   end
 end
--- spec/requests/apis/warehouse_api_spec.rb
+++ spec/requests/apis/warehouse_api_spec.rb
@@ -18,6 +18,53 @@ describe 'Warehouse API', type: :request do
       json_response = JSON.parse(response.body)
 
       expect(json_response['name']).to eq('Aeroporto SP')
+      expect(json_response['code']).to eq('GRU')
+      expect(json_response).not_to include('created_at')
+      expect(json_response).not_to include('updated_at')
+    end
+
+    it 'failure if warehouse not found' do
+      # Arrange
+
+      # Act
+      get '/api/v1/warehouses/99999999'
+
+      # Assert
+      expect(response.status).to eq(404)
+    end
+  end
+
+  context 'GET /api/v1/warehouses/' do
+    it 'lists all warehouses ordered by name' do
+      # Arrange
+      Warehouse.create!(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', state: 'RJ', area: 60_000,
+      address: 'Avenida do Porto, 1000', postal_code: '20015-510' , description: 'Galpão do Rio')
+      Warehouse.create!(name: 'Maceió', code: 'MCZ', city: 'Maceió', state: 'AL', area: 50_000,
+      address: 'Rua do Jacintinho, 75', postal_code: '75195-712', description: 'Galpão do setor industrial')
+
+      # Act
+      get '/api/v1/warehouses'
+
+      # Assert
+      expect(response.status).to eq 200
+      expect(response.content_type).to include 'application/json'
+      json_response = JSON.parse(response.body)
+      expect(json_response.length).to eq 2
+      expect(json_response[0]['name']).to eq 'Maceió'
+      expect(json_response[1]['name']).to eq 'Rio'
+    end
+
+    it 'empty if there is no warehouse' do
+      # Arrange
+
+      # Act
+      get '/api/v1/warehouses'
+
+      # Assert
+      expect(response.status).to eq 200
+      expect(response.content_type).to include 'application/json'
+      json_response = JSON.parse(response.body)
+      expect(json_response).to eq []
     end
   end
 end
